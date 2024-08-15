a1273cf8ed3ef83814b4247a5160be2538ebbcfd
Patience-Hellen <126111890+Patience-Hellen@users.noreply.github.com>
2024-08-15 17:04:07 +0300

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,10 @@
+name = input('What is your name?')
+print('Your name is ' + name)
+
+age = input('What is your age?')
+print('Your age is ' + age)
+
+location = input('What is your location?')
+print('Your location is ' + location)
+
+print('Hello ' + name + ', your age is ' + age + ' and your location is ' + location)
